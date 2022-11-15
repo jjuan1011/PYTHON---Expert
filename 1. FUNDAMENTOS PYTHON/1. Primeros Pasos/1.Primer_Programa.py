87f6615ae87bb561375d015c411838b58dd23301
juan <juanmeneses1111@gmail.com>
2022-11-15 13:46:05 -0500

    Empezamos el proyecto, La rama master englobará la carpeta PYTHON, de ahí para allá crearemos ramas para cada curso y subramas para cada clase de cada curso

---
+++ 1. FUNDAMENTOS PYTHON/1. Primeros Pasos/1.Primer_Programa.py
@@ -0,0 +1,11 @@
+
+
+# Esto es algo básico que debemos entener de python podemos imprimir mensajes y operaciones que en la terminal se verán resueltas
+print("Mamae el guevo")
+
+print("Imprimimos mensajes putito")
+
+print(2 + 4)
+print(3 - 8)
+print(8 / 5)
+print(7 * 5)
---
+++ 1. FUNDAMENTOS PYTHON/1. Primeros Pasos/2_Variables.py
@@ -0,0 +1,16 @@
+
+
+#  VARIABLES
+
+# Veamos las variables como cajas en las que  podemos almacenar muchos tipos de cosas, por ejemplo en una mudanza tenemos un camión con muchas cajas en las que almacenamos muchas cosas como juguetes, adornos, ropa, etc.
+
+my_name = "juan"
+print(my_name)
+
+my_age = 20
+print(my_age)
+
+# Tambien podems redefinir la misma variable de ahí su nombre ya que varía
+
+my_name = "geiny"
+print("my_name")
